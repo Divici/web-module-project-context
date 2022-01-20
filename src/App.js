ae810bcff6e67a7966c947e8b9c90055408b1c53
david-aihe <doa9200@gmail.com>
2022-01-20 14:58:48 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Contexts
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -20,17 +22,21 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
 			<ProductContext.Provider value={ {products, addItem} }>
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+				<CartContext.Provider value={cart}>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Navigation />
+
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
